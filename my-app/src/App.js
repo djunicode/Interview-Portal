4f791b74697f6c868635c47b1f2db55a42acce5b
Nevil Bhimani <nevilbhimani@Nevils-MacBook-Pro.local>
2022-04-05 01:03:06 +0530

    folder structure added in readme,login page layout

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Login_signup from "./pages/login_signup";
+function App() {
+  return (
+    <div className="App">
+      <Login_signup />
+    </div>
+  );
+}
+
+export default App;
