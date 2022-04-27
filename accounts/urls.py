2b3b740ff54f2dfb093682124afc876745d9c816
talkshrey <talkshrey@gmail.com>
2022-04-27 19:27:47 +0530

    changes made in url

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('interviewee_register/', views.IntervieweeRegisterAPI.as_view(), name = 'Interviewee Registration'),
-    # path('interviewer_register/', views.InterviewerRegisterAPI.as_view(), name = 'Interviewer Registration'),
     path('login/', views.LoginAPI.as_view(), name = 'login'),
 ]
